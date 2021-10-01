4a5ee5f412e235fd3eafeee969ba3760f2027b81
moran991231 <55349869+moran991231@users.noreply.github.com>
2021-10-01 17:58:26 +0900

    kivy test

---
+++ practice/MyApp.py
@@ -0,0 +1,15 @@
+
+# https://kivy.org/doc/stable/guide/basic.html
+import kivy
+
+kivy.require('1.0.6')
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+class MyApp(App):
+    def build (self):
+        return Label(text ='Hello World')
+
+if __name__ == '__main__':
+    MyApp().run()
